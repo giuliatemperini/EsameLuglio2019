e5f532851636276c8fd250b38993b832859cc90d
PaoloCacc <52297042+PaoloCacc@users.noreply.github.com>
2019-07-11 13:21:32 +0200

    Add files via upload
    Minor changes
    Added gson library to builldpath for JSON support

--- AppWebProject/src/main/java/com/appdeveloper/MobileAppApplication.java
+++ AppWebProject/src/main/java/com/appdeveloper/MobileAppApplication.java
@@ -14,8 +14,9 @@ import java.util.Vector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.app.controller.Element;
-import com.app.service.DownloadFile;
+import com.app.service.DataProcessor;
+import com.app.service.Element;
+
 
 @SpringBootApplication
 public class MobileAppApplication {
@@ -23,63 +24,13 @@ public class MobileAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppApplication.class, args);
 		
-		DownloadFile.JsonDecode("http://data.europa.eu/euodp/data/api/3/action/package_show?id=yGVKnIzbkC2ZHpT6jQouDg");
+		DataProcessor.jsonDecode("http://data.europa.eu/euodp/data/api/3/action/package_show?id=yGVKnIzbkC2ZHpT6jQouDg");
 		
 		
+		DataProcessor.csvParse("dataset.csv");
 		
+	
 		
-		int i=0;		
-		List<List<String>> records = new ArrayList<>();
-		
-		Vector<Element> v=new Vector<Element>();
-				
-		try (BufferedReader br = new BufferedReader(new FileReader("dataset.csv"))) {
-			
-			String line;
-			
-			while ((line = br.readLine()) != null && i<5000) {
-				if(i!=0) {
-				String[] values = line.split(",");
-				//System.out.println(values.length);
-				
-				if (values[6].isEmpty())					
-				{ 
-					values[6]="0";		 
-				}	
-				
-				records.add(Arrays.asList(values));									
-				if (values.length==8)
-				{
-				v.add(new Element(values[0], Integer.parseInt(values[1].substring(1, values[1].length()-1)), values[2].substring(1, values[2].length()-1), 
-						values[3].substring(1, values[3].length()-1), values[4].substring(1, values[4].length()-1),
-						values[5].substring(1, values[5].length()-1),
-						Float.parseFloat(values[6]),
-					values[7].substring(1, values[7].length()-1), null));				
-				}
-				
-				if (values.length==9)
-				{
-				v.add(new Element(values[0], Integer.parseInt(values[1].substring(1, values[1].length()-1)), values[2].substring(1, values[2].length()-1), 
-						values[3].substring(1, values[3].length()-1), values[4].substring(1, values[4].length()-1),
-						values[5].substring(1, values[5].length()-1),
-						Float.parseFloat(values[6]),
-					values[7].substring(1, values[7].length()-1), values[8].substring(1, values[8].length()-1)));
-				}				
-				System.out.println(" " +i);
-				}				
-				i++;				
-			}
-			
-			System.out.println(v.get(81).toString());
-			br.close();
-			
-			
-		} catch (IOException j) {
-			j.printStackTrace();
-			return;
-		}
-		
-		
-	}
 
+		}
 }
